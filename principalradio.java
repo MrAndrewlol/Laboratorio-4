45c3d8547c264e87367c1f6ea3a7b6f5b84def4b
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2022-11-11 18:22:06 -0600

    Update principalradio.java

--- principalradio.java
+++ principalradio.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 //Universidad del Valle de Guatemala
 //Andre Yatmian Jo Mai 22199
+//Esteban Meza 22252
 
 //Programa parecido a MAX solo que es mejor porque se llama Tienda latinoamreicana 
 //Amazon 2.0: Por lo tanto hay una categoria con sus respectivos productos y además de eso se pide en linea. 
@@ -10,9 +11,16 @@ public class principalradio{
     public static void main(String[] args){
         int opcion;
         int opciont;
+        int posicion;
+        String stringopcion;
+        float opcionfloat;
         boolean opcionbol = true;
         Scanner teclado = new Scanner(System.in);
         Radio radio = new Radio();
+        ModoReproduccion modoReproduccion = new ModoReproduccion();
+        ModoTelefono modoTelefono = new ModoTelefono();
+        Productividad productividad = new Productividad();
+        Telefono telefono = new Telefono();
        
 
 
@@ -57,6 +65,7 @@ public class principalradio{
                             System.out.println("\n1. Cambiar modo AM o FM");
                             System.out.println("2. Cambiar Emisoras");
                             System.out.println("3. Guardar emisora");
+                            System.out.println("4. Guardar emisora");
                             System.out.println("Que opcion desea realizar?");
                             opciont = teclado.nextInt();
 
@@ -118,6 +127,7 @@ public class principalradio{
                                             }
                                             case 3:{
                                                 System.out.println("Se ha salido de cambio de emisoras");
+                                                opcionbol = false;
                                                 break;
                                                 
                                             }
@@ -129,6 +139,18 @@ public class principalradio{
 
                                 }
                                 case 3:{
+                                    System.out.println("Guardando emisora: ");
+                                    modoradio.obtenerEmisoraActual();
+                                    modoradio.obtenerEmisorasGuardadas().add( modoradio.obtenerEmisoraActual());
+                                    break;
+
+                                }
+                                case 4:{
+                                    System.out.println("Que emisora desea cargar de sus emisoras guardadas? ");
+                                    opcionfloat = teclado.nextFloat();
+                                    teclado.nextLine();
+                                    modoradio.cargarEmisora( modoradio.obtenerPosicionEmisora(opcionfloat));
+                                    break;
 
                                 }
 
@@ -144,19 +166,163 @@ public class principalradio{
                             
                         }
 
-                        case 2:{
+                        case 2:{//Reproduccion
+
+                            System.out.println("1.Seleccionar lista de reproduccion");
+                            System.out.println("2.Cambiar cancion");
+                            System.out.println("3.Escuchar cancion");
+                            System.out.println("4.Salir");
+                            System.out.println("Que opcion desea realizar");
+                            opcion = teclado.nextInt();
+                            switch(opcion){
+                                case 1:{
+                                    System.out.println("Hay " + modoReproduccion.obtenerPlaylists().size() + " playlists");
+                                    System.out.println("Que numero de playlist desea usar?");
+                                    
+                                    posicion = teclado.nextInt();
+                                    modoReproduccion.seleccionarPlaylist(posicion);
+                                    System.out.println("Se esta reproduciendo actualmente.");
+
+
+                                    break;
+                                }
+
+                                case 2:{
+                                    System.out.println("1.Siguiente cancion");
+                                    System.out.println("2.Cancion anterior");
+                                    System.out.println("3.Salir");
+                                    opcion = teclado.nextInt();
+                                    switch(opcion){
+                                        case 1:{
+                                            System.out.println("Se reproduce la siguiente cancion");
+                                            modoReproduccion.siguienteCancion();
+
+
+                                        }
+
+                                        case 2:{
+                                            System.out.println("Se reproduce la cancion anterior");
+                                            modoReproduccion.cancionAnterior();
+
+                                        }
+                                        default:{
+                                            System.out.println("Se ha salido");
+                                            break;
+                                        }
+                                    }
+
+                                    
+                                    break;
+                                }
+                                
+                                case 3:{
+                                    System.out.println("Reproduciendo");
+                                    System.out.println(modoReproduccion.obtenerCancion().obtenerNombre() + modoReproduccion.obtenerCancion().obtenerAutor());                                   
+                                    System.out.println(modoReproduccion.obtenerCancion().obtenerDuracion()) ;  
+                                    break;
+                                }
+                                
+                                case 4:{
+                                    System.out.println("Saliendo...");
+                                    
+                                    break;
+                                }
+                                default:{
+                                    System.out.println("No se ingreso una opcion valida regresando");
+                                }
+                            }
+                            
+                            
+                            
 
                             break;
                             
                         }
 
-                        case 3:{
+                        case 3:{//Telefono
+                            System.out.println("1.Conectar telefono");
+                            System.out.println("2.Mostrar contactos");
+                            System.out.println("3.Llamar contactos");
+                            System.out.println("4.Finalizar llamada");
+                            System.out.println("5.Llamada en espera");
+                            System.out.println("6.Salir");
+                            opcion = teclado.nextInt();
+                            switch(opcion){
+
+                                case 1:{
+                                    System.out.println("Conectando telefono...");
+                                    modoTelefono.conectarTelefono(telefono);
+                                    telefono.obtenerNombre();
+                                    telefono.obtenerDueño();
+                                    telefono.obtenerNumero();
+
+                                    break;
+                                    
+                                    
+                                }
+                                case 2:{
+                                    System.out.println("Mostrando contactos..");
+                                    for (int i = 0; i <= telefono.obtenerContactos().size(); i++){
+
+                                        System.out.println((i+1)+". "+ telefono.obtenerContactos().get(i).obtenerNombre());
+                                        System.out.println(telefono.obtenerContactos().get(i).obtenerNumero());
+                                        System.out.println("\n");
+                                        
+                                    }
+
+                                    break;
+
+                                    
+                                }
+                                case 3:{
+
+                                    for (int i = 0; i <= telefono.obtenerContactos().size(); i++){
+
+                                        System.out.println((i+1)+". "+ telefono.obtenerContactos().get(i).obtenerNombre());
+                                        System.out.println(telefono.obtenerContactos().get(i).obtenerNumero());
+                                        System.out.println("\n");
+                                        
+                                    }
+
+                                    System.out.println("Que numero desea llamar?");
+                                    stringopcion = teclado.nextLine();
+                                    System.out.println("Llamando a " + stringopcion);
+
+                                    
+
+
+                                    break; 
+                                }
+                                case 4:{
+                                    System.out.println("Finalizando la llamada...");
+                                    break; 
+                                    
+                                }
+                                case 5:{
+                                    System.out.println("LLamada en espera....");
+                                    break; 
+
+                                    
+                                }
+                                case 6:{
+                                    modoTelefono.desconectarTelefono();
+                                    System.out.println("Saliendo...");
+                                    break; 
+                                }
+                            }
+
+                            
                             
 
                             break;
                         }
 
-                        case 4:{
+                        case 4:{//Productividad
+
+
+                            System.out.println("El pronostico del dia de hoy es..." + productividad.obtenerPronóstico());
+
+
 
 
 
