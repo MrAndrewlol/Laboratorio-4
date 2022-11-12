0c50ad438defce1a6f0f8b31432f771e34cd38e9
Andre <40283828+MrAndrewlol@users.noreply.github.com>
2022-11-11 18:17:31 -0600

    Update Cancion.java

--- Cancion.java
+++ Cancion.java
@@ -4,6 +4,9 @@
 
 //Interfaces del laboratorio 4 
 
+//Andre Yatmian Jo Mai 22199
+//Esteban Meza 22252
+
 import java.time.Duration;
 import java.util.Scanner;
 
@@ -15,10 +18,25 @@ public class Cancion implements ICancion  {
     Duration Duracion;
     String genero;
     String autor;
-
-    Cancion cancion = new Cancion();
     Scanner teclado = new Scanner(System.in);
 
+    
+
+    
+
+
+
+
+    public Cancion(String nombre, Duration Duracion, String genero, String autor) {
+        this.nombre = nombre;
+        this.Duracion = Duracion;
+        this.genero = genero;
+        this.autor = autor;
+    }
+    
+
+
+  
 
     /**
      * @return El nombre de la canción.
@@ -61,6 +79,7 @@ public class Cancion implements ICancion  {
     public String obtenerAutor(){
         System.out.println("Por favor ingresar el autor de la cancion");
         genero = teclado.nextLine();
-        return genero;
+        return autor;
     }
+
 }
